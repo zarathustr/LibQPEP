a3c517dc0d10d938946e318edae7f19c99eaefa0
Jin Wu <zarathustra@MacBook-Pro-2.local>
2020-11-06 17:38:35 +0800

    Update CMake and Compiling Options

--- cmake-build-debug/CMakeFiles/OMP_LIB.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/OMP_LIB.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake-build-debug/CMakeFiles/OMP_LIB.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for OMP_LIB.
-# This may be replaced when dependencies are built.
